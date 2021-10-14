8aa08b69624795a1d86fe516bdfdbb5c66e5e6b6
praveensajja01 <sajja.praveen@gmail.com>
2021-10-14 20:01:07 +0100

    remove buckets

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-resource "google_storage_bucket" "bucket" {
-  name = "test-bucket-random-001122"
-}
-
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
-}
-
 resource "google_storage_bucket" "gcs_bucket_new" {
   name = "test-bucket-random-new"
 }
