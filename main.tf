ba27015fff63d85be0705e13b3a2456299dc5212
Kethana Kolli <kethanakolli@Kethanas-MacBook-Air.local>
2021-10-14 19:27:43 +0100

    new gcs bucket

--- main.tf
+++ main.tf
@@ -5,3 +5,7 @@ resource "google_storage_bucket" "bucket" {
 resource "google_storage_bucket" "gcs_bucket" {
   name = "test-bucket-random-001123"
 }
+
+resource "google_storage_bucket" "gcs_bucket_new" {
+  name = "test-bucket-random-new"
+}
